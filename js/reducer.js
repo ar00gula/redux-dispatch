1e0fb92bc593f731c246833f47c741818cf30ec8
ar00gula <arianna.mayer@gmail.com>
2021-02-16 17:04:24 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -6,8 +6,18 @@ function changeState(state, action){
       return state;
   }
 }
+function dispatch(action){
+  state = changeState(state, action)
+  return state
+}
+
+function render(){
+  document.body.textContent = state.count
+}
 
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
-changeState(state, action)
+dispatch(action)
+dispatch(action)
+render()
